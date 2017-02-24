b7fd02f8c322d779a925d5f35174be858666b817
Brent Hughes <brentahughes@gmail.com>
2017-02-23 20:19:26 -0600

    Added docker file for cli

---
+++ decapod_cli.dockerfile
@@ -0,0 +1,26 @@
+FROM python:2.7
+
+ENV DECAPOD_API_TOKEN=26758c32-3421-4f3d-9603-e4b5337e7ecc
+ENV DECAPOD_API_ENDPOINT=http://10.10.10.10:9999
+
+RUN apt-get update \
+    && apt-get install -y git make \
+    && pip install setuptools \
+    && mkdir /app \
+    && mkdir /data
+
+RUN git clone --recurse-submodules https://github.com/Mirantis/ceph-lcm.git /decapod
+
+WORKDIR /decapod
+
+RUN chmod 0600 containerization/files/devconfigs/ansible_ssh_keyfile.pem \
+    && ssh-keygen -y -f containerization/files/devconfigs/ansible_ssh_keyfile.pem > ~/ansible_ssh_keyfile.pem.pub \
+    && chmod 0600 ~/ansible_ssh_keyfile.pem.pub 
+
+RUN make build_eggs \
+    && pip install output/eggs/decapodlib*.whl output/eggs/decapod_cli*.whl
+
+CMD rm -f /data/cloud-init-user-data \
+    && decapod -u $DECAPOD_API_ENDPOINT cloud-config \
+        $DECAPOD_API_TOKEN ~/ansible_ssh_keyfile.pem.pub > /data/cloud-init-user-data \
+    && sed -i '/metadata_ip = get_response/,+3d' /data/cloud-init-user-data
--- generate_cloud_init_data.sh
+++ generate_cloud_init_data.sh
@@ -4,41 +4,8 @@
 DECAPOD_API_ENDPOINT=http://10.10.10.10:9999
 DECAPOD_API_TOKEN=26758c32-3421-4f3d-9603-e4b5337e7ecc
 
-# Clone decapod if it isn't already'
-if [ ! -d "decapod" ]; then
-    git clone --recurse-submodules https://github.com/Mirantis/ceph-lcm.git decapod
-fi
-
-# Move into the decapod directory
-cd decapod
-
-# Install some prereqs
-sudo apt-get update
-sudo apt-get install -y python-pip make python-dev libffi-dev \
-  libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev
-
-pip install setuptools
-
-# Generate public key based on known private key
-chmod 0600 containerization/files/devconfigs/ansible_ssh_keyfile.pem
-ssh-keygen -y -f containerization/files/devconfigs/ansible_ssh_keyfile.pem > ~/ansible_ssh_keyfile.pem.pub
-chmod 0600 ~/ansible_ssh_keyfile.pem.pub
-
-# Build the decapod cli image
-if ! type "decapod" > /dev/null; then
-  make build_eggs
-  pip install output/eggs/decapodlib*.whl output/eggs/decapod_cli*.whl
-fi
-
-# Get the cloud-init user data for OS deployment
-echo "Generating cloud-init-user-data.new"
-rm -f cloud-init-user-data.new
-decapod -u $DECAPOD_API_ENDPOINT cloud-config \
-  $DECAPOD_API_TOKEN ~/ansible_ssh_keyfile.pem.pub > ./cloud-init-user-data.new
-
-# Remove some bad requests from user-data
-echo "Editing cloud-init-user-data.new"
-sed -i '/metadata_ip = get_response/,+3d' ./cloud-init-user-data.new
-
-cp -f cloud-init-user-data.new cloud-init-user-data
-rm -f cloud-init-user-data.new
+docker run --rm \
+  -e DECAPOD_API_ENDPOINT=$DECAPOD_API_ENDPOINT \
+  -e DECAPOD_API_TOKEN=$DECAPOD_API_TOKEN \
+  -v $(PWD):/data \
+  bah2830/decapod_cli
